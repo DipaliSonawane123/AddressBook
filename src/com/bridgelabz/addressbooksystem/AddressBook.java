d7f51a123a2d84b57467e9d216879bedcae9ef3b
Dipali <dipssonawane123@gmail.com>
2022-06-25 00:55:23 +0530

    Modified:Add New Contact modified

--- src/com/bridgelabz/addressbooksystem/AddressBook.java
+++ src/com/bridgelabz/addressbooksystem/AddressBook.java
@@ -1,8 +1,20 @@
 package com.bridgelabz.addressbooksystem;
 
 import java.util.ArrayList;
-
-public class AddressBook {
+import java.util.Iterator;
+public class AddressBook implements AddressBookIF {
     ArrayList<ContactPerson> contactList = new ArrayList<ContactPerson>();
+    @Override
+    public void addContact(ContactPerson person) {
+        contactList.add(person);
+    }
+    @Override
+    public void displayContents() {
+
+        Iterator<ContactPerson> iterator = contactList.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
 }
 
---
+++ src/com/bridgelabz/addressbooksystem/AddressBookIF.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.addressbooksystem;
+
+public interface AddressBookIF {
+    void addContact(ContactPerson person);
+
+    void displayContents();
+}
