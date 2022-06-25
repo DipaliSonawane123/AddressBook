415aae6b86876ac28c4573cf5131f94739249013
Dipali <dipssonawane123@gmail.com>
2022-06-26 04:19:53 +0530

    Modified:Dictionary feature modified

--- src/com/bridgelabz/addressbooksystem/AddressBookMain.java
+++ src/com/bridgelabz/addressbooksystem/AddressBookMain.java
@@ -1,18 +1,26 @@
 /*
-Ability to add multipleperson to Address Book
- - Use Console to add person details one at a time
-- Use Collection Class to maintain multiple contact persons in Address Book
+Refactor to add multiple
+Address Book to the
+System. Each Address Book
+has a unique Name - Use Console to add new Address Book - Maintain Dictionary of Address Book Name to
  */
 package com.bridgelabz.addressbooksystem;
 public class AddressBookMain {
 
     public static void main(String[] args) {
         System.out.println("---------- Welcome To Address Book Program!! ----------");
-
+        AddressBookDirectory addressBookDirectory = new AddressBookDirectory();
+        addressBookDirectory.operationDirectory();
         AddressBook addressBook = new AddressBook();
         addressBook.operation();
+
+
+
+
     }
-}
+
+    }
+
 
 
 
