16151ed3e856518a29021317c72c71eb4ba575d1
Dipali <dipssonawane123@gmail.com>
2022-07-14 09:07:59 +0530

    Find duplicate contact name feature is modified

--- src/com/bridgelabz/addressbooksystem/AddressBookMain.java
+++ src/com/bridgelabz/addressbooksystem/AddressBookMain.java
@@ -5,21 +5,32 @@ System. Each Address Book
 has a unique Name - Use Console to add new Address Book - Maintain Dictionary of Address Book Name to
  */
 package com.bridgelabz.addressbooksystem;
+
+import java.util.Scanner;
+
 public class AddressBookMain {
 
+    public static Scanner scannerObject = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("---------- Welcome To Address Book Program!! ----------");
-        AddressBookDirectory addressBookDirectory = new AddressBookDirectory();
+
+            System.out.println("---------- Welcome To Address Book Program!! ----------");
+            AddressBookDirectory addressBookDirectory = new AddressBookDirectory();
         addressBookDirectory.operationDirectory();
-        AddressBook addressBook = new AddressBook();
-        addressBook.operation();
+            AddressBook addressBook =new AddressBook();
+           addressBook.operation();
 
 
 
+        }
 
     }
 
-    }
+
+
+
+
+
 
 
 
